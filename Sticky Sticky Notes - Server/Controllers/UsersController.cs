2561800e0fceea8c3bb5140894c897a77171cbe2
DESKTOP-03RQ3MJ\jbilińskipl <jbilinskipl@gmail.com>
2017-11-02 19:42:25 +0100

    Added controller for adding new users.

---
+++ Sticky Sticky Notes - Server/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Sticky_Sticky_Notes___Server.Models;
+
+namespace Sticky_Sticky_Notes___Server.Controllers
+{
+    public class UsersController : ApiController
+    {
+        private DatabaseMainEntities _database = new DatabaseMainEntities();
+
+        // Add new user into db
+        public ResultItem Put([FromBody] UserItem userFromBody)
+        {
+            if (userFromBody.Username == null || userFromBody.Password == null)
+                return new ResultItem(false, "Null value on required field.");
+
+            if (userFromBody.Username.Length == 0 || userFromBody.Password.Length == 0)
+                return new ResultItem(false, "Empty value on required field.");
+
+            if (_database.Users.Where(u => u.Username == userFromBody.Username).ToList().Count != 0)
+                return new ResultItem(false, "User already exist.");
+
+            var user = new Users
+            {
+                Username = userFromBody.Username,
+                Password = userFromBody.Password
+            };
+            _database.Users.Add(user);
+            _database.SaveChanges();
+
+            return new ResultItem(true);
+        }
+    }
+}
