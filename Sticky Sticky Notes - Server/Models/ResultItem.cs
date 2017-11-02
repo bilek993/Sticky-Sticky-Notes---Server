17091da0189faef4c79342e84fa095c701611af6
DESKTOP-03RQ3MJ\jbilińskipl <jbilinskipl@gmail.com>
2017-11-02 19:32:51 +0100

    Added DTO for results.

---
+++ Sticky Sticky Notes - Server/Models/ResultItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sticky_Sticky_Notes___Server.Models
+{
+    public class ResultItem
+    {
+        public bool Successful { get; }
+        public string ErrorMessage { get; }
+
+        public ResultItem(bool successful, string errorMessage)
+        {
+            Successful = successful;
+            ErrorMessage = errorMessage;
+        }
+
+        public ResultItem(bool successful)
+        {
+            Successful = successful;
+            ErrorMessage = successful ? "OK" : "Unhandled exception.";
+        }
+    }
+}
