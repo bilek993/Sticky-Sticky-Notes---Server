f32e1f990d50dfc2d91f960ab90e8f40411be699
DESKTOP-03RQ3MJ\jbilińskipl <jbilinskipl@gmail.com>
2017-11-02 19:27:32 +0100

    Added two DTO items.

---
+++ Sticky Sticky Notes - Server/Models/NoteItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sticky_Sticky_Notes___Server.Models
+{
+    public class NoteItem
+    {
+        public string Context { get; set; }
+        public DateTime? LastEditDate { get; set; }
+    }
+}
---
+++ Sticky Sticky Notes - Server/Models/UserItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sticky_Sticky_Notes___Server.Models
+{
+    public class UserItem
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
