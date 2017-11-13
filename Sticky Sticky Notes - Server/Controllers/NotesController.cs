79fb9393964fda28db7de55cfd05c3b4be6a75e7
DESKTOP-03RQ3MJ\jbilińskipl <jbilinskipl@gmail.com>
2017-11-13 17:29:12 +0100

    Multiple notes can be updated.

--- Sticky Sticky Notes - Server/Controllers/NotesController.cs
+++ Sticky Sticky Notes - Server/Controllers/NotesController.cs
@@ -74,5 +74,31 @@ namespace Sticky_Sticky_Notes___Server.Controllers
                 return new ResultItem(true);
             }
         }
+
+        // Update multiple note
+        [BasicAuthentication]
+        public ResultItem Post([FromBody] List<NoteItem> notesToBeUpdated)
+        {
+            using (var database = new DatabaseMainEntities())
+            {
+                string username = Thread.CurrentPrincipal.Identity.Name;
+                var result = new ResultItem(true);
+
+                foreach (NoteItem noteToBeUpdated in notesToBeUpdated)
+                {
+                    Notes noteFromServer = database.Notes.FirstOrDefault(n => n.Id == noteToBeUpdated.Id
+                                                                              && n.Users.Username == username);
+
+                    if (noteFromServer == null)
+                        result = new ResultItem(false, "One of notes don't exist.");
+
+                    noteFromServer.Context = noteToBeUpdated.Context;
+                    noteFromServer.LastEditDate = noteToBeUpdated.LastEditDate;
+                    database.SaveChanges();
+                }
+
+                return result;
+            }
+        }
     }
 }
